553bdac23e94e75ec975aee434616f7af289413e
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-08 12:11:00 +0900

    コンポーネント適用

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import ButtonLink from "./_components/ButtonLink";
 
 type News = {
   id: string;
@@ -100,6 +101,9 @@ export default function Home() {
             </li>
           ))}
         </ul>
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
       </section>
     </>
   );
