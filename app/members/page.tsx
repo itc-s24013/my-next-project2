a14e2023216498fff91c3304d0427797fd8faca7
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-14 10:10:46 +0900

    メンバーのデータを表示する

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+
 const data = {
   contents: [
     {
@@ -41,8 +44,29 @@ const data = {
 
 export default function Page() {
   return (
-    <div style={{ margin: 20 }}>
-      <h1>メンバーページ</h1>
+    <div className={styles.container}>
+      {data.contents.length === 0 ? (
+        <p className={styles.empty}>メンバーが登録されていません。</p>
+      ) : (
+        <ul>
+          {data.contents.map((member) => (
+            <li key={member.id} className={styles.list}>
+              <Image
+                src={member.image.url}
+                alt=""
+                width={member.image.width}
+                height={member.image.height}
+                className={styles.image}
+              />
+              <dl>
+                <dt className={styles.name}>{member.name}</dt>
+                <dd className={styles.position}>{member.position}</dd>
+                <dd className={styles.profile}>{member.profile}</dd>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
