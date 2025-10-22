d3703b614d87ac44364494be868e0af3ec3708cd
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-22 10:46:01 +0900

    文言の修正

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page({ params }: Props) {
   return (
     <>
       <p>
-        <Category category={category} />
+        <Category category={category} /> の一覧
       </p>
       <NewsList news={news} />
     </>
