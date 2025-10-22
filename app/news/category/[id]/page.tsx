d88932c721b59154ff482b49bdd19b74fec9867d
itc-s24013 <s24013@std.it-college.ac.jp>
2025-10-22 10:38:30 +0900

    存在しないカテゴリーを入れるとnotFoundになる

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getNewsList } from "@/app/_libs/microcms";
+import { getCategoriyDetail, getNewsList } from "@/app/_libs/microcms";
+import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
 
 type Props = {
@@ -8,8 +9,10 @@ type Props = {
 };
 
 export default async function Page({ params }: Props) {
+  const category = await getCategoriyDetail(params.id).catch(notFound);
+
   const { contents: news } = await getNewsList({
-    filters: `category[equals]${params.id}`,
+    filters: `category[equals]${category.id}`,
   });
 
   return <NewsList news={news} />;
