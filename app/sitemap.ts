a41bdf45ca0cb0a95e06b2db6224e39600c65009
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-05 10:56:15 +0900

    sitemapにニュース詳細ページとカテゴリーを追加

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,21 @@
 import { MetadataRoute } from "next";
+import { getAllCategoryList, getAllNewsList } from "./_libs/microcms";
 
 const buildUrl = (path?: string) => `http://localhost:3000${path ?? ""}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const newsContents = await getAllNewsList();
+  const categoryContents = await getAllCategoryList();
+
+  const newsUrls: MetadataRoute.Sitemap = categoryContents.map((content) => ({
+    url: buildUrl(`/news/category/${content.id}`),
+    lastModified: content.revisedAt,
+  }));
+  const categoryUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
+    url: buildUrl(`/news/category/${content.id}`),
+    lastModified: content.revisedAt,
+  }));
+
   const now = new Date();
 
   return [
@@ -22,5 +35,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: buildUrl("/news"),
       lastModified: now,
     },
+    ...newsUrls,
+    ...categoryUrls,
   ];
 }
